28cc067398ecd17390953c140297432ad8e044fe
Max Aaronson <maxaaronson@gmail.com>
2018-02-25 02:31:12 -0500

    Changes for 2nd submission

--- NewsReportTool.py
+++ NewsReportTool.py
@@ -20,52 +20,71 @@ cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 print("The three most popular articles of all time are:\n")
 
 # Create the query that will be run on the data
-SQL = "select replace(replace(path, '/article/', ''), '-', ' ') as path, " +\
-      "count(*) as count from log where path != '/' " +\
-      "group by path order by count(*) desc limit 3;"
+SQL = """
+    select articles.title, count(*) as views
+    from articles, log
+    where log.path = concat('/article/', articles.slug)
+    group by articles.title
+    order by views desc
+    limit 3;
+      """
 
 # run the query on the db
 cursor.execute(SQL)
 
 # iterate through the output
 for row in cursor:
-    print("%s - %d views" % (row['path'], row['count'],))
+    print("%s - %d views" % (row['title'], row['views'],))
 
 print("\n\n")
 
 
 print("The most popular authors of all time are:\n")
 
-SQL = "select sum(A.num::int), authors.name from " +\
-      "(select replace(right(left(path, -1), -9), '-', ' ') " +\
-      "as title, count(*) as num from log where substr(path, 10) != '' " +\
-      "group by path order by count(*) desc) as A, articles, authors " +\
-      "where position(lower(A.title) in lower(articles.title)) > 0 " +\
-      "and articles.author = authors.id " +\
-      "group by authors.id;"
+SQL = """
+    select authors.name, sum(A.views::int) as views
+    from (select articles.title, count(*) as views
+          from articles, log
+          where log.path = concat('/article/', articles.slug)
+          group by articles.title
+          order by views desc) as A, authors, articles
+    where A.title = articles.title
+    and articles.author = authors.id
+    group by authors.name
+    order by views desc;
+      """
 
 cursor.execute(SQL)
 
 for row in cursor:
-    print("%s - %d views" % (row['name'], row['sum'],))
+    print("%s - %d views" % (row['name'], row['views'],))
 
 print("\n\n")
 
 print("more than 1% of requests lead to errors on these days:\n")
 
-SQL = "select A.time::date, A.count::float, B.count::float as count2, " +\
-      "(B.count/A.count::float)*100 as C from " +\
-      "(select time::date, count(*) as count from log group by " +\
-      "time::date order by count(*) desc) as A, " +\
-      "(select time::date, count(*) as count from log where status = " +\
-      "'404 NOT FOUND' group by time::date order by count(*) desc) as B " +\
-      "where A.time::date = B.time::date " +\
-      "and (B.count/A.count::float)*100 > 1 " +\
-      "order by C desc;"
+SQL = """
+    select A.time::date,
+    A.count::float,
+    B.count::float as count2,
+    (B.count/A.count::float)*100 as C
+    from (select time::date,
+          count(*) as count
+          from log
+          group by time::date
+          order by count(*) desc) as A,
+          (select time::date,
+           count(*) as count
+           from log
+           where status = '404 NOT FOUND'
+           group by time::date
+           order by count(*) desc) as B
+    where A.time::date = B.time::date
+    and (B.count/A.count::float)*100 > 1
+    order by C desc;
+      """
 
 cursor.execute(SQL)
 
 for row in cursor:
-    print("date: %s, total reqs: %d, errors: "
-          "%d, percent errors: %f" % (row['time'], row['count'],
-                                      row['count2'], row['c'],))
+    print("date: %s, percent errors: %f" % (row['time'], row['c'],))
--- readme.md
+++ readme.md
@@ -2,10 +2,18 @@
 This python program will perform several queries on the ```news``` database and display the outputs.
 
 ## Installation
-##### Required modules:
+##### Required environment:
+- python 3 interpreter installed
+- PostgreSQL installed with a database called ```news``` 
+
+##### Required python modules:
 - psycopg2
 - psycopg2.extras
 
+##### Required sql file:
+- the news.sql file must be run to populate the ```news``` database.  It can be found at https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+- run the file with ```psql -d news -f newsdata.sql```
+
 ##### To run:
-- Run ```"python NewsReportTool.py"``` from the command line
-    
+- Run ```"NewsReportTool"``` from the command line
+        
